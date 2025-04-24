9cb1460d13216efa1087e889692e5cf82171ae43
Thalya Stéffany <thalyacoiimbra@gmail.com>
2025-04-24 15:21:05 -0300

    feat(constants): creating auth constants

---
+++ app/src/constants/auth-contants.ts
@@ -0,0 +1,4 @@
+export const AUTH_BASE_URL = 'https://auth-api-rs23.onrender.com/api/auth'
+export const AUTH_LOGIN = '/login'
+export const AUTH_REGISTER ='/register'
+export const AUTH_REFRESH_TOKEN = '/refresh-token'
